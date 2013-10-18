ba8aec6d24596150f722ac6754e4f52af581caf0
Mei Wang <mwang@haivision.com>
2013-10-18 16:16:49 -0400

    setup

--- HaiWebdriver/setup.py
+++ HaiWebdriver/setup.py
@@ -10,17 +10,17 @@ from shutil import rmtree
 from ez_setup import use_setuptools
 use_setuptools()
 
-
-PACKAGE=None
-build = False
-
-if build:
-    assert PACKAGE, 'Please provide the package name (should start with Hai) to build distribution for!'
+PACKAGE = 'HaiSafari'
+VERSION = '1.1.0'
+AUTHOR = 'QA automation'
+EMAIL = 'qa_automation@hotmail.com'
+DESCRIPTION = 'Test Safari webdriver'
+INSTALL_REQUIRES = [ "HaiSafari == 1.1.0"]
 
 VERSION_FILE = '__version__.py'
 SOURCE_DIR   = 'src'
 
-execfile(join(dirname(__file__), SOURCE_DIR, PACKAGE, VERSION_FILE))
+execfile(join(dirname(__file__), SOURCE_DIR, 'HaiSafari', VERSION_FILE))
 
 
 setup(
@@ -33,7 +33,7 @@ setup(
     license      = 'free',
     py_modules   = ['ez_setup'],
     package_dir  = { '' : 'src'},
-    packages     = [PACKAGE],
+    packages     = ['HaiSafari'],
     package_data     = {PACKAGE: ['*/*.py', '*/*/*.py', '*/*/*/*.py']},
     install_requires = INSTALL_REQUIRES
     )
--- HaiWebdriver/src/HaiSafari/__version__.py
+++ HaiWebdriver/src/HaiSafari/__version__.py
@@ -1,9 +1,5 @@
 #! /usr/bin/python
 # -*- coding: UTF-8 -*-
 
-VERSION = '0.1.1'
-AUTHOR = 'Haivision QA automation'
-EMAIL = 'qa_automation@haivision.com'
+VERSION = '1.1.0'
 
-DESCRIPTION = 'Remote package, used for software installation & smoke test execution.'
-INSTALL_REQUIRES = ['fabric', 'pexpect']
